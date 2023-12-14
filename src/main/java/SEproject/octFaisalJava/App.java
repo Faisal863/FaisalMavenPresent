600e11354815a335827cf496053d9b58ab733745
Faisal <21bd1a665dcsmc@gmail.com>
2023-12-14 20:33:40 +0530

    Demo for team

---
+++ src/main/java/SEproject/octFaisalJava/App.java
@@ -0,0 +1,13 @@
+package SEproject.octFaisalJava;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Faisal" );
+    }
+}
